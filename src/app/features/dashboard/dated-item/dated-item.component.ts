556dbe06c380d87a9c30793a167e7fa13bcc64e3
Fuad <fuad.yusuf1@gmail.com>
2024-10-03 19:00:02 +0100

    03/10/2024_1900

--- src/app/features/dashboard/dated-item/dated-item.component.ts
+++ src/app/features/dashboard/dated-item/dated-item.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'dated-item',
   templateUrl: './dated-item.component.html',
-  styleUrls: ['./dated-item.component.css']
+  styleUrls: ['./dated-item.component.css'],
 })
 export class DatedItemComponent {
-
+  @Input() title: string = '';
+  @Input() main: string = '';
+  @Input() date: string = '';
+  @Input() bmi: string = '';
 }
--- src/app/features/dashboard/un-dated-item/un-dated-item.component.ts
+++ src/app/features/dashboard/un-dated-item/un-dated-item.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'unDated-item',
   templateUrl: './un-dated-item.component.html',
-  styleUrls: ['./un-dated-item.component.css']
+  styleUrls: ['./un-dated-item.component.css'],
 })
-export class UnDatedItemComponent {
+export class UnDatedItemComponent implements OnInit {
+  @Input() title: string = '';
+  @Input() main: string = '';
+  @Input() date: string = '';
+  @Input() bmi: number;
+  bmiStatus: string;
+
+  ngOnInit(): void {
+    if (this.bmi) {
+      if (this.bmi < 18.5) {
+        this.bmiStatus = 'underweight'
+      }     
+      if (this.bmi >= 18.5 && this.bmi <= 24.9) {
+        this.bmiStatus = 'healthy';
+      }     
+      if (this.bmi >= 25 && this.bmi <= 29.9) {
+        this.bmiStatus = 'overweight';
+      }     
+      if (this.bmi >= 30 && this.bmi <= 39.9) {
+        this.bmiStatus = 'underweight';
+      }   
+    }
+  }
 
 }
