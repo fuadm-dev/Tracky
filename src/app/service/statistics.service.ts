b176ed9b1fd966b435b328af33f3644f37011b52
Fuad <fuad.yusuf1@gmail.com>
2024-10-13 14:00:02 +0100

    13/10/2024_1400

--- src/app/service/statistics.service.ts
+++ src/app/service/statistics.service.ts
@@ -4,20 +4,35 @@ import { User } from '../shared/models/user';
 import { Statistics } from '../shared/models/statistics';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StatisticsService implements OnInit {
+  constructor(private bmiService: BmiService) {}
 
-  constructor(private bmiService: BmiService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    
+  buildStatistics(user: User): Statistics {
+    let stats: Statistics;
+    stats.startWeight = user.start.weight;
+    stats.targetWeight = user.target.weight;
+    stats.predictedWeight;
+    this.setCurrentWeight(user);
+    return stats;
   }
 
-  getStatistics(user: User){
-   let stats: Statistics;
-
-   
+  setCurrentWeight(user: User) {
+    if (user.record.weightLogs.length > 0) {
+      user.current = user.record.weightLogs[user.record.weightLogs.length - 1];
+    } else {
+      user.current = user.start;
+    }
   }
 
+  calcPredictedWeight(user: User) {}
+  calcBmi(user: User) {}
+  calcChangeBmi(user: User) {}
+  calcChangeWeight(user: User) {}
+  calcTime(user: User) {}
+  calcProgress(user: User) {}
+  calcOntarget(user: User) {}
 }
