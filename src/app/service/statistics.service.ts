25319e643479e94a3522cb2f61c0d15b5d5be57e
Fuad <fuad.yusuf1@gmail.com>
2024-10-13 17:00:02 +0100

    13/10/2024_1700

--- src/app/service/statistics.service.ts
+++ src/app/service/statistics.service.ts
@@ -1,31 +1,25 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BmiService } from './bmi.service';
 import { User } from '../shared/models/user';
 import { Statistics } from '../shared/models/statistics';
-import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class StatisticsService implements OnInit {
-  user: User;
+export class StatisticsService {
+  stats: Statistics;
+  
+  constructor(private bmiService: BmiService) {}
 
-  constructor(private userService: UserService, private bmiService: BmiService) {}
-
-  ngOnInit(): void {
-    this.user = this.userService.getUser();
-  }
-
-  buildStatistics(user): Statistics {
-    let stats: Statistics;
-    stats.startWeight = user.start.weight;
-    stats.targetWeight = user.target.weight;
-    stats.predictedWeight;
+  buildStatistics(user: User): Statistics {
+    this.stats.startWeight = user.start.weight;
+    this.stats.targetWeight = user.target.weight;
+    this.stats.predictedWeight;
     this.setCurrentWeight(user);
-    return stats;
+    return this.stats;
   }
 
-  setCurrentWeight(user) {
+  setCurrentWeight(user: User) {
     if (user.record.weightLogs.length > 0) {
       user.current = user.record.weightLogs[user.record.weightLogs.length - 1];
     } else {
@@ -33,11 +27,15 @@ export class StatisticsService implements OnInit {
     }
   }
 
-  calcPredictedWeight(user) {}
-  calcBmi(user) {}
-  calcChangeBmi(user) {}
-  calcChangeWeight(user) {}
-  calcTime(user) {}
-  calcProgress(user) {}
-  calcOntarget(user) {}
+  setStartWeight(){}
+
+  setTargetWeight(){}
+
+  calcPredictedWeight(user: User) {}
+  calcBmi(user: User) {}
+  calcChangeBmi(user: User) {}
+  calcChangeWeight(user: User) {}
+  calcTime(user: User) {}
+  calcProgress(user: User) {}
+  calcOntarget(user: User) {}
 }
