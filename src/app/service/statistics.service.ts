b7a379e050e8619b50cc44a16b2e8119a7fb4988
Fuad <fuad.yusuf1@gmail.com>
2024-10-13 02:00:02 +0100

    13/10/2024_0200

--- src/app/service/statistics.service.ts
+++ src/app/service/statistics.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { BmiService } from './bmi.service';
+import { User } from '../shared/models/user';
+import { Statistics } from '../shared/models/statistics';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StatisticsService {
+export class StatisticsService implements OnInit {
 
-  constructor() { }
+  constructor(private bmiService: BmiService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  getStatistics(user: User){
+   let stats: Statistics;
+
+   
+  }
+
 }
