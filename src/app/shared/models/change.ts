9c2baa9802bacd557bf16462b659893e3a45b424
Fuad <fuad.yusuf1@gmail.com>
2024-10-21 08:48:13 +0100

    working

--- src/app/shared/models/change.ts
+++ src/app/shared/models/change.ts
@@ -1,9 +1,4 @@
 export class Change {
   weightChange: number = 0;
   bmiChange: number = 0;
-
-  // constructor(weightChange:number, bmiChange: number) {
-  //   this.weightChange = weightChange;
-  //   this.bmiChange = bmiChange;
-  // }
 }
