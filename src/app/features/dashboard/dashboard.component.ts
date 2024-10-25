17873755f0590b16c448b5658d702bdf71d14ddf
Fuad <fuad.yusuf1@gmail.com>
2024-10-26 00:00:02 +0100

    26/10/2024_0000

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { StatisticsService } from 'src/app/service/statistics.service';
 import { UserService } from 'src/app/service/user.service';
 import { IDashItem } from 'src/app/shared/models/idash-item';
 import { StatsClass } from 'src/app/shared/models/stats-class';
@@ -13,18 +14,52 @@ import { User } from 'src/app/shared/models/user';
 export class DashboardComponent implements OnInit {
   user: User;
   userStats: StatsClass;
-  elapsedWeeks: number;
-  
-  dashItems: IDashItem[];
+  elapsedWeekAsPercent: number;
 
-  constructor(private userService: UserService) {}
+  dashItems: IDashItem[] = [];
+
+  constructor(
+    private userService: UserService,
+    private statsService: StatisticsService
+  ) {}
 
   ngOnInit() {
     this.user = this.userService.getUser();
-    this.userStats = this.userService.getUser().userStats;
-    this.elapsedWeeks = Math.round(
+    this.statsService.buildStats(this.user);
+    this.userStats = this.user.userStats;
+
+    this.elapsedWeekAsPercent = Math.round(
       (this.userStats.elapsedTime.weeks / this.userStats.totalTime.weeks) * 100
     );
-    console.log(this.user.userStats);    
+
+    this.buildDashItems(this.user)
+    console.log(this.user.userStats);
+    console.log(this.dashItems);
+  }
+
+  buildDashItems(user:User){
+    // build Start-Item
+    const startItem: IDashItem = {
+      title: 'Start',
+      units: 'kg',
+      height: this.user.height,
+      weight: user.userStats.start.weight,
+      date: user.userStats.start.date,
+      calendar: true,
+      bmiComponent: true
+    };
+
+    // build Current-Item
+    const currentItem: IDashItem = {
+      title: 'Current',
+      units: 'kg',
+      height: this.user.height,
+      weight: user.userStats.current.weight,
+      date: user.userStats.current.date,
+      calendar: true,
+      bmiComponent: true
+    };
+
+    this.dashItems.push(startItem, currentItem)
   }
 }
