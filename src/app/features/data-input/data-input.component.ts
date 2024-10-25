bc808b9c3dd9be400fd63be2f0d45cd509f84b3d
Fuad <fuad.yusuf1@gmail.com>
2024-10-25 01:19:41 +0100

    working

--- src/app/features/data-input/data-input.component.ts
+++ src/app/features/data-input/data-input.component.ts
@@ -1,16 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { InputService } from 'src/app/service/input.service';
 import { User } from 'src/app/shared/models/user';
+import { Weight } from 'src/app/shared/models/weight';
 
 @Component({
   selector: 'data-input',
   templateUrl: './data-input.component.html',
   styleUrls: ['./data-input.component.css'],
 })
-export class DataInputComponent {
+export class DataInputComponent implements OnInit {
   @Input() user: User;
+  test: Weight = new Weight();
 
   constructor(private inputService: InputService) {}
-
-
+  ngOnInit(): void {
+    this.user.record.weightLogs.push(this.test);
+  }
 }
