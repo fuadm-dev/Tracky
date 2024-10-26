971f6f1c3734c475598a12c9483044d7bc9a4412
Fuad <fuad.yusuf1@gmail.com>
2024-10-27 00:00:02 +0100

    27/10/2024_0000

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { DashItemBuilderService } from 'src/app/service/dash-item-builder.service';
 import { StatisticsService } from 'src/app/service/statistics.service';
 import { UserService } from 'src/app/service/user.service';
 import { IDashItem } from 'src/app/shared/models/idash-item';
@@ -14,107 +15,26 @@ import { User } from 'src/app/shared/models/user';
 export class DashboardComponent implements OnInit {
   user: User;
   userStats: StatsClass;
-  elapsedWeekAsPercent: number;
-
   dashItems: IDashItem[] = [];
 
   constructor(
     private userService: UserService,
-    private statsService: StatisticsService
+    private statsService: StatisticsService,
+    private dashBuilder: DashItemBuilderService
   ) {}
 
   ngOnInit() {
     this.user = this.userService.getUser();
     this.statsService.buildStats(this.user);
     this.userStats = this.user.userStats;
+    
+    this.statsService.buildStats(this.user);
 
-    this.elapsedWeekAsPercent = Math.round(
-      (this.userStats.elapsedTime.weeks / this.userStats.totalTime.weeks) * 100
-    );
-
-    this.buildDashItems(this.user);
     console.log(this.user.userStats);
     console.log(this.dashItems);
   }
 
-  buildDashItems(user: User) {
-    // build Start-Item
-    const startItem: IDashItem = {
-      title: 'Start',
-      units: 'kg',
-      weight: user.userStats.start.weight,
-      date: user.userStats.start.date,
-      calendar: true,
-      bmiComponent: true,
-    };
-
-    // build Current-Item
-    const currentItem: IDashItem = {
-      title: 'Current',
-      units: 'kg',
-      weight: user.userStats.current.weight,
-      date: user.userStats.current.date,
-      calendar: true,
-      bmiComponent: true,
-    };
-
-    // build Target-Item
-    const targetItem: IDashItem = {
-      title: 'Target',
-      units: 'kg',
-      weight: user.userStats.target.weight,
-      date: user.userStats.target.date,
-      calendar: true,
-      bmiComponent: true,
-    };
-
-    // build Change-Item
-    const changeItem: IDashItem = {
-      title: 'Change',
-      units: 'kg',
-      weight: user.userStats.change.weightChange,
-      calendar: false,
-      bmiComponent: true,
-    };
-
-    // build Predicted-Item
-    const predictedItem: IDashItem = {
-      title: 'Predicted',
-      units: 'kg',
-      weight: user.userStats.predicted.weight,
-      date: user.userStats.predicted.date,
-      calendar: true,
-      bmiComponent: true,
-    };
-
-    // build Progress-Item
-    const progressItem: IDashItem = {
-      title: 'Progress Made',
-      units: '%',
-      weight: user.userStats.pctProgress,
-      progress: user.userStats.pctProgress,
-      calendar: false,
-      bmiComponent: false,
-    };
-
-    // build Time-Item
-    const timeItem: IDashItem = {
-      title: 'Elapsed Time',
-      units: '%',
-      weight: this.elapsedWeekAsPercent,
-      progress: this.elapsedWeekAsPercent,
-      calendar: false,
-      bmiComponent: false,
-    };
-
-    this.dashItems.push(
-      startItem,
-      currentItem,
-      targetItem,
-      changeItem,
-      predictedItem,
-      progressItem,
-      timeItem
-    );
+  reBuildDashboard(){
+    this.dashBuilder.buildDashItems(this.user, this.dashItems);
   }
 }
---
+++ src/app/service/dash-item-builder.service.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@angular/core';
+import { User } from '../shared/models/user';
+import { IDashItem } from '../shared/models/idash-item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DashItemBuilderService {
+  constructor() {}
+
+  buildDashItems(user: User, dashItemsArr: IDashItem[]) {
+    // build Start-Item
+    let elapsedWeekAsPercent: number = Math.round(
+      (user.userStats.elapsedTime.weeks / user.userStats.totalTime.weeks) * 100
+    );
+
+    const startItem: IDashItem = {
+      title: 'Start',
+      units: 'kg',
+      weight: user.userStats.start.weight,
+      date: user.userStats.start.date,
+      calendar: true,
+      bmiComponent: true,
+    };
+
+    // build Current-Item
+    const currentItem: IDashItem = {
+      title: 'Current',
+      units: 'kg',
+      weight: user.userStats.current.weight,
+      date: user.userStats.current.date,
+      calendar: true,
+      bmiComponent: true,
+    };
+
+    // build Target-Item
+    const targetItem: IDashItem = {
+      title: 'Target',
+      units: 'kg',
+      weight: user.userStats.target.weight,
+      date: user.userStats.target.date,
+      calendar: true,
+      bmiComponent: true,
+    };
+
+    // build Change-Item
+    const changeItem: IDashItem = {
+      title: 'Change',
+      units: 'kg',
+      weight: user.userStats.change.weightChange,
+      calendar: false,
+      bmiComponent: true,
+    };
+
+    // build Predicted-Item
+    const predictedItem: IDashItem = {
+      title: 'Predicted',
+      units: 'kg',
+      weight: user.userStats.predicted.weight,
+      date: user.userStats.predicted.date,
+      calendar: true,
+      bmiComponent: true,
+    };
+
+    // build Progress-Item
+    const progressItem: IDashItem = {
+      title: 'Progress Made',
+      units: '%',
+      weight: user.userStats.pctProgress,
+      progress: user.userStats.pctProgress,
+      calendar: false,
+      bmiComponent: false,
+    };
+
+    // build Time-Item
+    const timeItem: IDashItem = {
+      title: 'Elapsed Time',
+      units: '%',
+      weight: elapsedWeekAsPercent,
+      progress: elapsedWeekAsPercent,
+      calendar: false,
+      bmiComponent: false,
+    };
+
+    dashItemsArr.push(
+      startItem,
+      currentItem,
+      targetItem,
+      changeItem,
+      predictedItem,
+      progressItem,
+      timeItem
+    );
+  }
+}
