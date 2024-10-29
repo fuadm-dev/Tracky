3cffd55afcd6a059be5075718bdf1007c1cfec10
Fuad <fuad.yusuf1@gmail.com>
2024-10-29 11:50:10 +0000

    created target class & target service

--- src/app/service/set-on-target.service.ts
+++ src/app/service/set-on-target.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@angular/core';
+import { UserService } from './user.service';
+import { User } from '../shared/models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SetOnTargetService {
+  user: User;
+  constructor(private userService: UserService) {}
 
-  constructor() { }
+  ngOnInit(): void {
+    this.user = this.userService.getUser();
+  }
 
-  setOnTargetValue
+  setOnTargetValue() {
+    let onTarget = {
+      isOnTarget: true,
+      message: '',
+    };
+
+    this.user.userStats.onTarget = this.user.userStats.onTarget;
+    console.log(this.user.userStats);
+
+    if (this.user.userStats.onTarget) {
+      onTarget.message = 'On Target!';
+    } else onTarget.message = 'Off Target!';
+  }
 }
--- src/app/shared/models/target.ts
+++ src/app/shared/models/target.ts
@@ -1,5 +1,8 @@
-export interface Target {
-  targetDate: string;
-  targetWeight: number;
-  targetBMI: number;
+import { IBmi } from './bmi';
+
+export class Target {
+  date?: Date = new Date();
+  weight: number = 0;
+  bmi?: IBmi = { bmi: 0, bmiStatus: '' };
+  target: Target
 }
