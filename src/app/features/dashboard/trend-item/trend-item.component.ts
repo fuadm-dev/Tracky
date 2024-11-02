54ac40cb7ee9c67171bb10517da5f5493a702368
Fuad <fuad.yusuf1@gmail.com>
2024-11-02 16:00:02 +0000

    02/11/2024_1600

--- src/app/features/dashboard/trend-item/trend-item.component.ts
+++ src/app/features/dashboard/trend-item/trend-item.component.ts
@@ -6,22 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./trend-item.component.css'],
 })
 export class TrendItemComponent {
-  labels = Utils.months({ count: 7 });
-  data = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1,
-      },
-    ],
-  };
+//   labels = Utils.months({ count: 7 });
+//   data = {
+//     labels: labels,
+//     datasets: [
+//       {
+//         label: 'My First Dataset',
+//         data: [65, 59, 80, 81, 56, 55, 40],
+//         fill: false,
+//         borderColor: 'rgb(75, 192, 192)',
+//         tension: 0.1,
+//       },
+//     ],
+//   };
 
- public config = {
-    type: 'line',
-    data: data,
-  };
+//  public config = {
+//     type: 'line',
+//     data: data,
+//   };
 }
