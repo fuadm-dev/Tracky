469956f8e76e1d8bf8de46337006da54dee8eb07
Fuad <fuad.yusuf1@gmail.com>
2024-11-04 14:00:02 +0000

    04/11/2024_1400

--- src/app/service/chart.service.ts
+++ src/app/service/chart.service.ts
@@ -13,31 +13,22 @@ export class ChartService {
   data = [];
 
   getMonths(user: User) {
-    const userWeightLogs = user.record.weightLogs;
-    let year: Weight[][] = [
-      [],
-      [],
-      [],
-      [],
-      [],
-      [],
-      [],
-      [],
-      [],
-      [],
-      [],
-      [],
-    ]
+    let userWeightLogs = user.record.weightLogs;
+    let year: Weight[][] = [[], [], [], [], [], [], [], [], [], [], [], []];
 
+    //Sort logs in date order
+    userWeightLogs = userWeightLogs.sort(
+      (a, b) => b.date.getTime() - a.date.getTime()
+    );
+    console.log(userWeightLogs);
 
-
+    //Group logs by months into year array
     for (let i = 0; i < userWeightLogs.length; i++) {
       const weight = userWeightLogs[i];
       year[weight.date.getMonth()].push(weight);
     }
 
+    //Return
     console.log(year);
-    
   }
-
 }
