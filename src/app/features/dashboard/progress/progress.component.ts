65a7b639a3c273900ad179cc647faf4dabbbf6f2
Fuad <fuad.yusuf1@gmail.com>
2024-11-12 23:53:36 +0000

    dougnut partially implemented

--- src/app/features/dashboard/progress/progress.component.ts
+++ src/app/features/dashboard/progress/progress.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { _Time } from 'src/app/shared/models/_time';
 import { User } from 'src/app/shared/models/user';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'progress-item',
@@ -8,7 +9,37 @@ import { User } from 'src/app/shared/models/user';
   styleUrls: ['./progress.component.css'],
 })
 export class ProgressComponent {
-  user:User;
-  @Input() title: string = '';
+  user: User;
+  @Input() title: string = 'Progress';
   @Input() progress: number;
+  chart: any;
+
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart() {
+    this.chart = new Chart('MyChart', {
+      type: 'doughnut', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels: ['lost', ''],
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: [75, 25],
+            backgroundColor: [
+              'green',
+              'grey'
+            ],
+            hoverOffset: 4,
+          },
+        ],
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }
 }
