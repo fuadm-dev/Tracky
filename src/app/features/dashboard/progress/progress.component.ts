07b16516bc6f4ac06d9fcb7ee95f7feb29805c67
Fuad <fuad.yusuf1@gmail.com>
2024-11-13 09:00:01 +0000

    13/11/2024_0900

--- src/app/features/dashboard/progress/progress.component.ts
+++ src/app/features/dashboard/progress/progress.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { _Time } from 'src/app/shared/models/_time';
 import { User } from 'src/app/shared/models/user';
 import Chart from 'chart.js/auto';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'progress-item',
@@ -10,12 +11,24 @@ import Chart from 'chart.js/auto';
 })
 export class ProgressComponent {
   user: User;
-  @Input() title: string = 'Goal Progress';
-  @Input() progress: number;
+  title: string = 'Goal Progress';
+  progress: number;
   chart: any;
 
+  constructor(private userService: UserService) {}
+
   ngOnInit(): void {
+    this.user = this.userService.getUser();
     this.createChart();
+    this.progress = this.chart.data.datasets[0].data[0],
+    console.log(this.chart.data.datasets[0].data[0]);
+  }
+
+  getWeightLossProgress(): number[] {
+    let progressArr: number[] = [];
+    progressArr.push(this.user.userStats.pctProgress);
+    progressArr.push(100 - this.user.userStats.pctProgress);
+    return progressArr;
   }
 
   createChart() {
@@ -24,11 +37,11 @@ export class ProgressComponent {
 
       data: {
         // values on X-Axis
-        labels: ['lost', ''],
+        // labels: ['lost', 'left'],
         datasets: [
           {
-            label: 'My First Dataset',
-            data: [75, 25],
+            label: 'Progress',
+            data: this.getWeightLossProgress(),
             backgroundColor: ['green', 'grey'],
             hoverOffset: 4,
           },
