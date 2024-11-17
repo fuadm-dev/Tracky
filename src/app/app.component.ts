dc7142cdf71422a25c5a61ace88cca9091a0599b
Fuad <fuad.yusuf1@gmail.com>
2024-11-17 14:00:02 +0000

    17/11/2024_1400

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { UserService } from './service/user.service';
 import { User } from './shared/models/user';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { Weight } from './shared/models/weight';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,12 +20,17 @@ export class AppComponent implements OnInit {
   selectedWeightLog: Weight;
   checked: boolean = true;
 
+  currentRoute: string;
+
   @ViewChild(DashboardComponent) dashboardComponent: DashboardComponent;
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
     this.user = this.userService.getUser();
+    this.currentRoute = this.router.url
+    console.log(this.router.url);
+    
   }
 
   refreshData(): void {
